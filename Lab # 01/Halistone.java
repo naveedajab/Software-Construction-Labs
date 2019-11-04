4af4b1626c24cfe5272e23f81edae3c63a24a6c9
naveedajab <57361731+naveedajab@users.noreply.github.com>
2019-11-04 23:58:10 +0500

    Update Halistone.java

--- Lab # 01/Halistone.java
+++ Lab # 01/Halistone.java
@@ -1,16 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package sclab1;
 
-import java.util.Scanner;
-
-/**
- *
- * @author fa16-bse-081
- */
 public class Halistone {
     public static void main(String args[]){
         int n=0;
